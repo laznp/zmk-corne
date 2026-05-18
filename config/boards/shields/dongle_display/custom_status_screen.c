ab642929ee422c90c8bb8ed97aac33883e9eb4bf
Lazuardi N Putra <me@laznp.id>
2026-05-18 23:52:30 +0700

    main update

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -47,11 +47,6 @@ lv_style_t global_style;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
-
-    /* Rotate 90° for vertical display orientation.
-     * Change to LV_DISP_ROT_270 if text is upside down. */
-    lv_disp_set_rotation(lv_disp_get_default(), LV_DISP_ROT_90);
-
     screen = lv_obj_create(NULL);
 
     lv_style_init(&global_style);
@@ -62,13 +57,25 @@ lv_obj_t *zmk_display_status_screen() {
     lv_style_set_text_letter_space(&global_style, 1);
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
-    
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_BATTERY)
+    zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
+    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_LAYER)
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+#endif
 
 #if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_WPM)
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align_to(zmk_widget_wpm_status_obj(&wpm_status_widget), zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_OUT_RIGHT_MID, 7, 0);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
@@ -83,22 +90,6 @@ lv_obj_t *zmk_display_status_screen() {
     zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
     lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -2);
 #endif
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_LAYER)
-    zmk_widget_layer_status_init(&layer_status_widget, screen);
-#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
-    lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 5);
-#else
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, -3);
-#endif
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_BATTERY)
-    zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
-    lv_obj_align_to(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget),
-                    zmk_widget_output_status_obj(&output_status_widget),
-                    LV_ALIGN_OUT_BOTTOM_LEFT, 0, 2);
 #endif
 
     return screen;
