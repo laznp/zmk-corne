026809f1de8a936c4a35195f1ed841ec7727e321
Lazuardi N Putra <me@laznp.id>
2026-05-19 23:04:25 +0700

    main fix: black bg, white text, revert overlay

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -44,7 +44,11 @@ lv_obj_t *zmk_display_status_screen() {
 
     screen = lv_obj_create(NULL);
 
+    lv_obj_set_style_bg_color(screen, lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
+
     lv_style_init(&global_style);
+    lv_style_set_text_color(&global_style, lv_color_white());
     lv_style_set_text_font(&global_style, &lv_font_unscii_8);
     lv_style_set_text_letter_space(&global_style, 1);
     lv_style_set_text_line_space(&global_style, 1);
