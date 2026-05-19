649127f25dba6280d7b8c4912e62483fe206cb6b
Lazuardi N Putra <me@laznp.id>
2026-05-19 23:09:42 +0700

    main test: explicit screen size 128x32

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -44,6 +44,8 @@ lv_obj_t *zmk_display_status_screen() {
 
     screen = lv_obj_create(NULL);
 
+    lv_obj_set_size(screen, 128, 32);
+
     lv_obj_set_style_bg_color(screen, lv_color_black(), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
 
