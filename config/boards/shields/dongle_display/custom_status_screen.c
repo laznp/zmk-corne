47a3a122a6d6316dd9709cf9ca770ef75b7be46f
Lazuardi N Putra <me@laznp.id>
2026-05-20 01:21:52 +0700

    main update

---
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "custom_status_screen.h"
+#include "widgets/battery_status.h"
+#include "widgets/layer_status.h"
+#include "widgets/output_status.h"
+#include "widgets/hid_indicators.h"
+#include "widgets/wpm_status.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+static struct zmk_widget_output_status output_status_widget;
+
+#if IS_ENABLED(CONFIG_ZMK_BATTERY)
+static struct zmk_widget_dongle_battery_status dongle_battery_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_LAYER)
+static struct zmk_widget_layer_status layer_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+static struct zmk_widget_hid_indicators hid_indicators_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_WPM)
+static struct zmk_widget_wpm_status wpm_status_widget;
+#endif
+
+lv_style_t global_style;
+
+lv_obj_t *zmk_display_status_screen() {
+    lv_obj_t *screen;
+
+    screen = lv_obj_create(NULL);
+
+    lv_style_init(&global_style);
+    lv_style_set_bg_color(&global_style, lv_color_white());
+    lv_style_set_bg_opa(&global_style, LV_OPA_COVER);
+    lv_style_set_text_color(&global_style, lv_color_black());
+    lv_style_set_text_font(&global_style, &lv_font_unscii_8);
+    lv_style_set_text_letter_space(&global_style, 1);
+    lv_style_set_text_line_space(&global_style, 1);
+    lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
+
+    // Portrait layout: 32px wide x 128px tall
+
+    // output_status: top of screen
+    zmk_widget_output_status_init(&output_status_widget, screen);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+
+#if IS_ENABLED(CONFIG_ZMK_BATTERY)
+    // battery: stacked per-peripheral, icon at right edge, label to its left
+    zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
+    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget),
+                 LV_ALIGN_TOP_RIGHT, 0, 22);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_WPM)
+    // wpm: vertically stacked icon+label, centered horizontally, upper-middle area
+    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_TOP_MID, 0, 65);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+    // hid_indicators: above layer name
+    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
+    lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_BOTTOM_LEFT, 0,
+                 -10);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_LAYER)
+    // layer: bottom of screen
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+#endif
+
+    return screen;
+}
