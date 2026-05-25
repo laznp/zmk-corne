e665d1873ed41ef425dd3bdf797396da9f6c7428
Lazuardi N Putra <me@laznp.id>
2026-05-26 02:04:09 +0700

    main update

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -5,26 +5,9 @@
  */
 
 #include "custom_status_screen.h"
-#include "widgets/output_status.h"
-#include "widgets/bongo_cat.h"
-#include "widgets/hid_indicators.h"
-#include "widgets/wpm_status.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
-static struct zmk_widget_output_status output_status_widget;
-#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
-static struct zmk_widget_bongo_cat bongo_cat_widget;
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-static struct zmk_widget_hid_indicators hid_indicators_widget;
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_WPM)
-static struct zmk_widget_wpm_status wpm_status_widget;
-#endif
 
 lv_style_t global_style;
 
@@ -36,31 +19,15 @@ lv_obj_t *zmk_display_status_screen() {
     lv_style_set_bg_opa(&global_style, LV_OPA_COVER);
     lv_style_set_text_color(&global_style, lv_color_black());
     lv_style_set_text_font(&global_style, &lv_font_unscii_8);
-    lv_style_set_text_letter_space(&global_style, 1);
-    lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
 
-    zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_obj_t *top = lv_label_create(screen);
+    lv_label_set_text(top, "Lazu's");
+    lv_obj_align(top, LV_ALIGN_TOP_MID, 0, 4);
 
-    lv_obj_t *label_mid = lv_label_create(screen);
-    lv_label_set_text(label_mid, "Laz");
-    lv_obj_align(label_mid, LV_ALIGN_LEFT_MID, 26, 0);
-
-#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
-    zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
-    lv_obj_set_pos(zmk_widget_bongo_cat_obj(&bongo_cat_widget), 78, 3);
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_WPM)
-    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_TOP_LEFT, 22, 0);
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
-    lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_BOTTOM_LEFT, 22, 0);
-#endif
+    lv_obj_t *bot = lv_label_create(screen);
+    lv_label_set_text(bot, "Property");
+    lv_obj_align(bot, LV_ALIGN_BOTTOM_MID, 0, -4);
 
     return screen;
 }
